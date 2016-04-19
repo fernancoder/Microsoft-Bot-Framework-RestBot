51075f715e35ba5791e1b4d6f82c0674263aaa91
Fernando Martín <fernancoder@gmail.com>
2016-04-19 22:11:22 +0200

    Update RestBot.js

--- RestBot.js
+++ RestBot.js
@@ -5,21 +5,20 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var collection = require('../dialogs/DialogCollection');
 var session = require('../Session');
-var storage = require('../storage/Storage');
+var consts = require('../consts');
+var request = require('request');
 var uuid = require('node-uuid');
-var readline = require('readline');
-var TextBot = (function (_super) {
-    __extends(TextBot, _super);
-    function TextBot(options) {
+var RestBot = (function (_super) {
+    __extends(RestBot, _super);
+    function RestBot(options) {
         _super.call(this);
         this.options = {
-            maxSessionAge: 14400000,
             defaultDialogId: '/',
             minSendDelay: 1000
         };
         this.configure(options);
     }
-    TextBot.prototype.configure = function (options) {
+    RestBot.prototype.configure = function (options) {
         if (options) {
             for (var key in options) {
                 if (options.hasOwnProperty(key)) {
@@ -27,143 +26,206 @@ var TextBot = (function (_super) {
                 }
             }
         }
-    };
-    TextBot.prototype.beginDialog = function (address, dialogId, dialogArgs) {
-        if (!this.hasDialog(dialogId)) {
-            throw new Error('Invalid dialog passed to TextBot.beginDialog().');
-        }
-        var message = address || {};
-        var userId = message.to ? message.to.address : 'user';
-        this.dispatchMessage(userId, message, null, dialogId, dialogArgs, true);
-    };
-    TextBot.prototype.processMessage = function (message, callback) {
-        this.emit('message', message);
-        if (!message.id) {
-            message.id = uuid.v1();
-        }
-        if (!message.from) {
-            message.from = { channelId: 'text', address: 'user' };
-        }
-        this.dispatchMessage(message.from.address, message, callback, this.options.defaultDialogId, this.options.defaultDialogArgs);
-    };
-    TextBot.prototype.listenStdin = function () {
+    };    
+    RestBot.prototype.listen = function (options) {
         var _this = this;
-        function onMessage(message) {
-            console.log(message.text);
-        }
-        this.on('reply', onMessage);
-        this.on('send', onMessage);
-        this.on('quit', function () {
-            rl.close();
-            process.exit();
-        });
-        var rl = readline.createInterface({ input: process.stdin, output: process.stdout, terminal: false });
-        rl.on('line', function (line) {
-            _this.processMessage({ text: line || '' });
-        });
-    };
-    TextBot.prototype.dispatchMessage = function (userId, message, callback, dialogId, dialogArgs, newSessionState) {
-        var _this = this;
-        if (newSessionState === void 0) { newSessionState = false; }
-        var ses = new session.Session({
-            localizer: this.options.localizer,
-            minSendDelay: this.options.minSendDelay,
-            dialogs: this,
-            dialogId: dialogId,
-            dialogArgs: dialogArgs
-        });
-        ses.on('send', function (reply) {
-            _this.saveData(userId, ses.userData, ses.sessionState, function () {
-                if (reply && reply.text) {
-                    if (callback) {
-                        callback(null, reply);
-                        callback = null;
+        this.configure(options);
+        return function (req, res) {
+            if (req.body) {
+                _this.dispatchMessage(null, req.body, _this.options.defaultDialogId, _this.options.defaultDialogArgs, res);
+            }
+            else {
+                var requestData = '';
+                req.on('data', function (chunk) {
+                    requestData += chunk;
+                });
+                req.on('end', function () {
+                    try {
+                        var msg = JSON.parse(requestData);
+                        _this.dispatchMessage(null, msg, _this.options.defaultDialogId, _this.options.defaultDialogArgs, res);
                     }
-                    else if (message.id || message.conversationId) {
-                        reply.from = message.to;
-                        reply.to = reply.replyTo || reply.to;
-                        reply.conversationId = message.conversationId;
+                    catch (e) {
+                        _this.emit('error', new Error('Mensaje Bot Framework inválido'));
+                        res.send(400);
+                    }
+                });
+            }
+        };
+    };
+    RestBot.prototype.dispatchMessage = function (userId, message, dialogId, dialogArgs, res) {
+        var _this = this;
+        try {
+            if (!message || !message.type) {
+                this.emit('error', new Error('Invalid Bot Framework Message'));
+                return res ? res.send(400) : null;
+            }
+            /*if (!userId) {
+                if (message.from && message.from.id) {
+                    userId = message.from.id;
+                }
+                else {
+                    this.emit('error', new Error('Invalid Bot Framework Message'));
+                    return res ? res.send(400) : null;
+                }
+            }*/
+
+            var sessionId;
+            if (message.botConversationData && message.botConversationData[consts.Data.SessionId]) {
+                sessionId = message.botConversationData[consts.Data.SessionId];
+            }
+            else {
+                sessionId = uuid.v1();
+                message.botConversationData = message.botConversationData || {};
+                message.botConversationData[consts.Data.SessionId] = sessionId;
+            }
+
+            this.emit(message.type, message);
+            if (message.type == 'Message') {
+
+                var ses = new session.Session({
+                    localizer: this.options.localizer,
+                    minSendDelay: this.options.minSendDelay,
+                    dialogs: this,
+                    dialogId: dialogId,
+                    dialogArgs: dialogArgs
+                });
+                ses.on('send', function (reply) {
+
+                    console.log("reply:");
+                    console.log(reply);
+
+                    reply = reply || {};
+                    reply.botConversationData = message.botConversationData;
+                    if (reply.text && !reply.language && message.language) {
                         reply.language = message.language;
-                        _this.emit('reply', reply);
+                    }
+                    var data = {
+                        userData: ses.userData,
+                        conversationData: ses.conversationData,
+                        perUserConversationData: ses.perUserInConversationData
+                    };
+                    data.perUserConversationData[consts.Data.SessionState] = ses.sessionState;
+
+                    _this.saveData(userId, sessionId, data, reply, function (err) {
+                        if (res) {
+                            _this.emit('reply', reply);
+                            res.send(200, reply);
+                            res = null;
+                        }                        
+                    });
+                });
+                ses.on('error', function (err) {
+                    _this.emit('error', err, ses.message);
+                    if (res) {
+                        res.send(500);
+                    }
+                });
+                ses.on('quit', function () {
+                    _this.emit('quit', ses.message);
+                });
+                this.getData(userId, sessionId, message, function (err, data) {
+                    if (!err) {
+                        var sessionState;
+                        ses.userData = data.userData || {};
+                        ses.conversationData = data.conversationData || {};
+                        ses.perUserInConversationData = data.perUserConversationData || {};
+                        if (ses.perUserInConversationData.hasOwnProperty(consts.Data.SessionState)) {
+                            sessionState = ses.perUserInConversationData[consts.Data.SessionState];
+                            delete ses.perUserInConversationData[consts.Data.SessionState];
+                        }
+                        ses.dispatch(sessionState, message);
                     }
                     else {
-                        _this.emit('send', reply);
+                        _this.emit('error', err, message);
                     }
+                });
+            }
+            else if (res) {
+                var msg;
+                switch (message.type) {
+                    case "botAddedToConversation":
+                        msg = this.options.groupWelcomeMessage;
+                        break;
+                    case "userAddedToConversation":
+                        msg = this.options.userWelcomeMessage;
+                        break;
+                    case "endOfConversation":
+                        msg = this.options.goodbyeMessage;
+                        break;
                 }
-            });
-        });
-        ses.on('error', function (err) {
-            if (callback) {
-                callback(err, null);
-                callback = null;
+                res.send(msg ? { type: message.type, text: msg } : {});
             }
-            else {
-                _this.emit('error', err, message);
-            }
-        });
-        ses.on('quit', function () {
-            _this.emit('quit', message);
-        });
-        this.getData(userId, function (err, userData, sessionState) {
-            if (!err) {
-                ses.userData = userData || {};
-                ses.dispatch(newSessionState ? null : sessionState, message);
-            }
-            else {
-                _this.emit('error', err, message);
-            }
-        });
-    };
-    TextBot.prototype.getData = function (userId, callback) {
-        var _this = this;
-        if (!this.options.userStore) {
-            this.options.userStore = new storage.MemoryStorage();
         }
-        if (!this.options.sessionStore) {
-            this.options.sessionStore = new storage.MemoryStorage();
+        catch (e) {
+            this.emit('error', e instanceof Error ? e : new Error(e.toString()));
+            res.send(500);
         }
-        var ops = 2;
-        var userData, sessionState;
-        this.options.userStore.get(userId, function (err, data) {
-            if (!err) {
-                userData = data;
-                if (--ops == 0) {
-                    callback(null, userData, sessionState);
-                }
-            }
-            else {
-                callback(err, null, null);
-            }
-        });
-        this.options.sessionStore.get(userId, function (err, data) {
-            if (!err) {
-                if (data && (new Date().getTime() - data.lastAccess) < _this.options.maxSessionAge) {
-                    sessionState = data;
-                }
-                if (--ops == 0) {
-                    callback(null, userData, sessionState);
-                }
-            }
-            else {
-                callback(err, null, null);
-            }
-        });
     };
-    TextBot.prototype.saveData = function (userId, userData, sessionState, callback) {
-        var ops = 2;
-        function onComplete(err) {
-            if (!err) {
-                if (--ops == 0) {
+    RestBot.prototype.getData = function (userId, sessionId, msg, callback) {
+        var botPath = '/' + this.options.appId;
+        var userPath = botPath + '/users/' + userId;
+        var convoPath = botPath + '/conversations/' + sessionId;
+        var perUserConvoPath = botPath + '/conversations/' + sessionId + '/users/' + userId;
+        var ops = 3;
+        var data = {};
+        function load(id, field, store, botData) {
+            data[field] = botData;
+            if (store) {
+                store.get(id, function (err, item) {
+                    if (callback) {
+                        if (!err) {
+                            data[field] = item;
+                            if (--ops == 0) {
+                                callback(null, data);
+                            }
+                        }
+                        else {
+                            callback(err, null);
+                            callback = null;
+                        }
+                    }
+                });
+            }
+            else if (callback && --ops == 0) {
+                callback(null, data);
+            }
+        }
+        load(userPath, 'userData', this.options.userStore, msg.botUserData);
+        load(convoPath, 'conversationData', this.options.conversationStore, msg.botConversationData);
+        load(perUserConvoPath, 'perUserConversationData', this.options.perUserInConversationStore, msg.botPerUserInConversationData);
+    };
+    RestBot.prototype.saveData = function (userId, sessionId, data, msg, callback) {
+        var botPath = '/' + this.options.appId;
+        var userPath = botPath + '/users/' + userId;
+        var convoPath = botPath + '/conversations/' + sessionId;
+        var perUserConvoPath = botPath + '/conversations/' + sessionId + '/users/' + userId;
+        var ops = 3;
+        function save(id, field, store, botData) {
+            if (store) {
+                store.save(id, botData, function (err) {
+                    if (callback) {
+                        if (!err && --ops == 0) {
+                            callback(null);
+                        }
+                        else {
+                            callback(err);
+                            callback = null;
+                        }
+                    }
+                });
+            }
+            else {
+                msg[field] = botData;
+                if (callback && --ops == 0) {
                     callback(null);
                 }
             }
-            else {
-                callback(err);
-            }
         }
-        this.options.userStore.save(userId, userData, onComplete);
-        this.options.sessionStore.save(userId, sessionState, onComplete);
+        save(userPath, 'botUserData', this.options.userStore, data.userData);
+        save(convoPath, 'botConversationData', this.options.conversationStore, data.conversationData);
+        save(perUserConvoPath, 'botPerUserInConversationData', this.options.perUserInConversationStore, data.perUserConversationData);
     };
-    return TextBot;
+    return RestBot;
 })(collection.DialogCollection);
-exports.TextBot = TextBot;
+exports.RestBot = RestBot;
+
